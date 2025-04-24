267d41ac3f5f50ceea3a8c572c5e62bac5b2c10c
Dmitry Yakovlev <100353974+DmitryYak@users.noreply.github.com>
2025-04-24 10:42:14 +0300

    Update Java_stepik.java

--- Java_stepik.java
+++ Java_stepik.java
@@ -380,5 +380,19 @@ class MyNumber {
     }
 }
 
+//На вход подаются два числа: x и y.
+Выведите на печать значение выражения
+import java.util.Scanner;
+class MyNumber {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = ((x + 1) / (y - 1)) + ((y + 1) / (x - 1));
+        System.out.println(z);
+        
+   }
+}
+
 
 
